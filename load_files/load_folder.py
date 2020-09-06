f28a4de0de5d7ff585342fda67009f56673ae5d2
leschenkos <leschenkoslava@gmai.com>
2020-09-05 22:21:40 -0400

    readme and interface description

--- load_files/load_folder.py
+++ load_files/load_folder.py
@@ -6,7 +6,7 @@ sys.path.append(Path)
 Pi=np.pi
 from constants import c
 import classes.error_class as ER
-
+from scipy import ndimage
 
 def imp_dir(Dir,xcal='nm',filenames=True):
     """importing spectrum, calibrating to angular frequency (if input is in nm)
@@ -22,9 +22,9 @@ def imp_dir(Dir,xcal='nm',filenames=True):
         Spout=Sp
     return Spout
 
-def imp_spec(FP,xcal='nm',normalize=True,axis=(0,1)):
+def imp_spec(FP,xcal='nm',normalize=False,axis=(0,1)):
     """importing spectrum, calibrating to angular frequency (if input is in nm)
-    return field"""
+    return [angular frequency in 2*Pi*Hz, normalized field (sqrt(intensity))]"""
     try:
         if FP[-4:] == 'spec':
             """load akspec file"""
@@ -35,7 +35,7 @@ def imp_spec(FP,xcal='nm',normalize=True,axis=(0,1)):
         elif FP[-3:] == 'txt' or FP[-3:] == 'dat': #standard case with a pure 2 colomn data
             if open(FP,'r').readline() == 'Wavelength\tIntensity\n':
                 """file saved by a Wavescan (two column)"""
-                Sp=np.loadtxt(FP,skiprows=1)
+                Sp=Wavescan(FP,xout='nm')
             else: 
                 L=np.fromstring(open(FP,'r').readline(),sep='\t')
                 if len(L) == 2:
@@ -46,6 +46,7 @@ def imp_spec(FP,xcal='nm',normalize=True,axis=(0,1)):
             """load Sfrogger spectrum"""
             axis=(0,1)
             Sp=np.loadtxt(FP,skiprows=1)
+            
             xcal='PHz'            
         else:
             raise ER.SL_exception('unknown fundamental spectrum file format')
@@ -53,20 +54,28 @@ def imp_spec(FP,xcal='nm',normalize=True,axis=(0,1)):
         raise ER.ReadError(er)
     else:
         if xcal=='nm':
-            Sp1=np.array([[2*Pi*c/s[axis[0]]*10**9,np.sqrt(np.abs(s[axis[1]]))] for s in Sp])
+            Sp1=Sp[:,(axis[0],axis[1])]
+            Sp1[:,0]=2*Pi*c/Sp1[:,0]*10**9
+            Sp1[:,1]=np.abs(Sp1[:,1])**0.5
             Sp1=Sp1[::-1]
         elif xcal=='THz':
-            Sp1=np.array([[2*Pi*s[axis[0]]*10**12,np.sqrt(np.abs(s[axis[1]]))] for s in Sp])
+            Sp1=Sp[:,(axis[0],axis[1])]
+            Sp1[:,0]=2*Pi*Sp1[:,0]*10**12
+            Sp1[:,1]=np.abs(Sp1[:,1])**0.5
         elif xcal=='PHz':
-            Sp1=np.array([[2*Pi*s[axis[0]]*10**15,np.sqrt(np.abs(s[axis[1]]))] for s in Sp])
+            Sp1=Sp[:,(axis[0],axis[1])]
+            Sp1[:,0]=2*Pi*Sp1[:,0]*10**15
+            Sp1[:,1]=np.abs(Sp1[:,1])**0.5
         else:
             print("unknown x calibration")
             raise ER.CalibrationError(FP)
+        if normalize:
+            Sp1[:,1]=Sp1[:,1]/(Sp1[:,1]).max()
         return Sp1
 
 def imp_phase(FP,xcal='nm',normalize=True,axis=(0,1)):
     """importing spectrum, calibrating to angular frequency (if input is in nm)
-    return field"""
+    return [angular frequency in 2*Pi*Hz, phase in rad]"""
     try:
         if FP[-10:] == 'PhSpectrum':
             """load Sfrogger phase"""
@@ -88,3 +97,72 @@ def imp_phase(FP,xcal='nm',normalize=True,axis=(0,1)):
         else:
             print("unknown x calibration")
         return Sp1
+    
+def Wavescan(FP,Filter=True,xout='um',SubstractBackground=True,Lam_background=[[0,0.1]]):
+    """import and filter Wavescan data
+    Lam_background is a list of ranges to be used for background calculation
+    (if all are out of the data range, the background is calculated from the edges of the spectrum)"""
+    Sp0=np.loadtxt(FP,skiprows=1)
+    Y=Sp0[:,1]
+    Y=Y/Y.max()
+    
+    if xout=='um':
+        X=Sp0[:,0]*10**-3 #x in um
+    elif xout=='nm':
+        X=Sp0[:,0] #x in nm
+    # print(X)
+    if Filter:
+        """filter out the scanning artifacts (mudulations)"""
+        #Gaussian filter
+        sigma=2 #standard deviation for Gaussian kernel
+        Y1=ndimage.gaussian_filter1d(Y,sigma)
+    if SubstractBackground:
+        #===remove background
+        Xmin=X.min()
+        Xmax=X.max()
+        Lbg=np.array(Lam_background)
+        #remove wrong borders for the background calculation
+        ind_min=Lbg<Xmin
+        Lbg[ind_min]=Xmin
+        ind_max=Lbg>Xmax
+        Lbg[ind_max]=Xmax
+        ind0=[np.logical_and(X>=l[0],X<=l[1]) for l in Lbg]
+        if len(ind0) > 1:
+            ind=np.sum(np.array(ind0),axis=0)
+        else:
+            ind=ind0[0]
+        if np.sum(ind) < 3:
+            #get background from both edges
+            ind[:]=False
+            ind[:9]=True
+            ind[-10:]=True
+        # print(np.sum(ind))
+        Bg=np.sum(Y1[ind])/np.sum(ind)
+        # print(Bg)
+    Y1-=Bg
+    #zero values below 0
+    ind=Y1<0
+    Y1[ind]=0
+    Sp=np.ones((len(Y1),2))
+    Sp[:,0]=X
+    Sp[:,1]=Y1
+    return Sp
+
+def imp_time(FP,xcal='fs',normalize=True,axis=(0,1)):
+    """import temporal pulse profile"""
+    
+    try:
+        if FP[-7:] == 'IntTime':
+            """load Sfrogger data"""
+            axis=(0,1)
+            Sp=np.loadtxt(FP,skiprows=1)
+        
+        else:
+            raise ER.SL_exception('unknown temporal file format')
+    except OSError as er:
+        raise ER.ReadError(er)
+    else:
+        if xcal=='fs':
+            return Sp
+        else:
+            print("unknown x calibration")
---
+++ readme.txt
@@ -0,0 +1,29 @@
+(c) Vyacheslav (Slava) Leshchneko 
+leschenkoslava@gmail.com
+
+%main features
+The program is for pulse reconstruction from SHG-FROG using PCGPA algorithm.
+Latest ideas from Trebino's team are implemented in the code including better first spectrum guess and 
+multi-grid algorithm (option to choose) [Opt. Express 26, 2643-2649 (2018), Opt. Express 27, 2112-2124 (2019)]
+
+The program is written in Python. The interface in Qt. (I have only tested it on Windows with Python3)
+Therefore, check that you have the PyQt5 and pyqtgraph packages. In principle, try to run, and errors will indicate which package is missing if any.
+
+An automated binned is included, which allows almost on fly reconstruction of raw experimental data. 
+(according to multiple real life tests, it seems to be working well with at least roughly compressed pulses.)
+
+% some instructions
+A number of input file formats can be used (though most of them are specific to our lab data acquisition software)
+The standard (from free Trebino's soft) *.frg file format is also included.
+a generic FROG raw data file format that is accepted by the program should have .txt extension, and the following structure
+1st line: delays
+2d line: wavelengths in nm
+following lines: data, so that each line is a spectrum for different delays (mast not be though, since there is a transpose option in the interface)
+(you can also add your file format to load_frog function in PCGPA.py)
+
+fundamental spectrum can also have multiple formats (but they are also specific to our lab data acquisition software)
+a generic spectrum data file format that is accepted by the program should have .txt or .dat extension, 
+and the two-column structure with the first column being wavelength in nm and the second column being intensity
+
+Do not use 'parallel' optional box if you are not sure that you really need it (it runs multigrid algorithm using multiprocessing). 
+Python, after one of updates in 2020, started automatically parallelizing map() function; thus, 'parallel' options seems to be unnecessary any more
